0eab915ae0371f1fa53b42d6092600142bb14bca
Walnut <the.ultimate.nut69@gmail.com>
2022-01-31 15:16:38 +0100

    created a small somewhat usable website

---
+++ react-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <><p class = "text">some react.js stuff goes here</p></>
+console.log(element);
+
+ReactDOM.render(element, document.getElementById("root"));
